f3b9e1360eabaafbd336a68e57916baf085f8ba3
miquelg3 <miquelg3@gmail.com>
2023-12-03 20:33:11 +0100

    Rotar aún no funciona perfectamente, las coordenadas ahora están más claras

--- Assets/Scripts/Tetris.cs
+++ Assets/Scripts/Tetris.cs
@@ -26,18 +26,18 @@ public class Tetris : MonoBehaviour
         }
         posiciones = new bool[columnas, 20];
         // Tablero
-        for (int i = 21; i > 0; i--)
+        for (int y = 19; y > - 2; y--)
         {
             GameObject cuboTablero = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cuboTablero.transform.position = new Vector3(0, i, 0);
+            cuboTablero.transform.position = new Vector3(-1, y, 0);
             GameObject cuboTableroFinal = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cuboTableroFinal.transform.position = new Vector3(columnas + 1, i, 0);
-            if (i == 1)
+            cuboTableroFinal.transform.position = new Vector3(columnas, y, 0);
+            if (y == -1)
             {
-                for (int x = 1; x < columnas + 1; x++)
+                for (int x = -1; x < columnas; x++)
                 {
                     GameObject cuboLineaFinal = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    cuboLineaFinal.transform.position = new Vector3(x, i, 0);
+                    cuboLineaFinal.transform.position = new Vector3(x, y, 0);
                 }
             }
         }
@@ -51,7 +51,7 @@ public class Tetris : MonoBehaviour
     {
         // Para que no traspase la pared ni las demás piezas
         GameObject piezaDerecha = null;
-        float posicionMasDerecha = 0;
+        float posicionMasDerecha = -1;
 
         GameObject piezaIzquierda = null;
         float posicionMasIzquierda = columnas;
@@ -79,7 +79,7 @@ public class Tetris : MonoBehaviour
             }
         }
         // Mover derecha
-        if (Input.GetKeyUp(derecha) && piezaDerecha.transform.position.x < columnas && !posiciones[(int)piezaDerecha.transform.position.x, (int)piezaDerecha.transform.position.y - 2])
+        if (Input.GetKeyUp(derecha) && PoderIrDerecha(piezaDerecha))
         {
             foreach (var pieza in piezas)
             {
@@ -87,7 +87,7 @@ public class Tetris : MonoBehaviour
             }
         }
         // Mover a izquierda
-        if (Input.GetKeyUp(izquierda) && piezaIzquierda.transform.position.x > 1 && !posiciones[(int)piezaIzquierda.transform.position.x - 2, (int)piezaIzquierda.transform.position.y - 2])
+        if (Input.GetKeyUp(izquierda) && PoderIrIzquierda(piezaIzquierda))
         {
             foreach (var pieza in piezas)
             {
@@ -95,7 +95,7 @@ public class Tetris : MonoBehaviour
             }
         }
         // Mover abajo
-        if (Input.GetKeyUp(abajo) && piezaAbajo.transform.position.y > 2 && !posiciones[(int)piezaAbajo.transform.position.x - 1, (int)piezaAbajo.transform.position.y - 3])
+        if (Input.GetKeyUp(abajo) && PoderIrAbajo(piezaAbajo))
         {
             foreach (var pieza in piezas)
             {
@@ -105,7 +105,14 @@ public class Tetris : MonoBehaviour
         // Rotar pieza
         if (Input.GetKeyUp(rotar))
         {
-            RotarPieza();
+            Vector3[] posicionesAlRotar = RotarPieza();
+            if (PoderRotarPieza(posicionesAlRotar))
+            {
+                for (int i = 0; i < posicionesAlRotar.Length; i++)
+                {
+                    piezas[i].transform.position = posicionesAlRotar[i];
+                }
+            }
         }
     }
 
@@ -136,11 +143,12 @@ public class Tetris : MonoBehaviour
 
     IEnumerator CaerPieza()
     {
-        while (poderIrAbajo())
+        while (PoderIrAbajo())
         {
             foreach (var pieza in piezas)
             {
                 pieza.transform.position = new Vector3(pieza.transform.position.x, pieza.transform.position.y - 1, 0);
+                Debug.Log($"Posición {pieza.transform.position.x}, {pieza.transform.position.y}");
             }
             yield return new WaitForSeconds(tiempoMovimiento);
         }
@@ -148,27 +156,45 @@ public class Tetris : MonoBehaviour
         // Llega abajo, guarda posición y spawnea nueva pieza
         foreach (var pieza in piezas)
         {
-            posiciones[(int)pieza.transform.position.x - 1, (int)pieza.transform.position.y - 2] = true;
+            posiciones[(int)pieza.transform.position.x, (int)pieza.transform.position.y] = true;
             GameObject nuevaPosicion = GameObject.CreatePrimitive(PrimitiveType.Cube);
             nuevaPosicion.transform.position = pieza.transform.position;
-            Debug.Log($"Posición {pieza.transform.position.x - 1}, {pieza.transform.position.y - 2}");
+            Debug.LogWarning($"Posición {pieza.transform.position.x}, {pieza.transform.position.y}");
         }
         SpawnPieza();
     }
 
-    bool poderIrAbajo()
+    bool PoderIrAbajo()
     {
         foreach (var pieza in piezas)
         {
-            int x = (int)pieza.transform.position.x - 1;
-            int y = (int)pieza.transform.position.y - 2;
+            int x = (int)pieza.transform.position.x;
+            int y = (int)pieza.transform.position.y - 1;
 
-            if (y <= 0 || posiciones[x, y - 1])
+            if (y < 0 || posiciones[x, y])
             {
                 return false;
             }
         }
         return true;
+    }
+
+    bool PoderIrDerecha(GameObject piezaDerecha)
+    {
+        if (piezaDerecha.transform.position.x < columnas && !posiciones[(int)piezaDerecha.transform.position.x + 1, (int)piezaDerecha.transform.position.y]) return true;
+        else return false;
+    }
+
+    bool PoderIrIzquierda(GameObject piezaIzquierda)
+    {
+        if (piezaIzquierda.transform.position.x > 1 && !posiciones[(int)piezaIzquierda.transform.position.x - 1, (int)piezaIzquierda.transform.position.y]) return true;
+        else return false;
+    }
+
+    bool PoderIrAbajo(GameObject piezaAbajo)
+    {
+        if (piezaAbajo.transform.position.y > 2 && !posiciones[(int)piezaAbajo.transform.position.x, (int)piezaAbajo.transform.position.y - 1]) return true;
+        else return false;
     }
 
     // Comprobar si una línea está completa
@@ -183,54 +209,69 @@ public class Tetris : MonoBehaviour
     // Piezas
     void SpawnS()
     {
-        piezas[0].transform.position = new Vector3(columnas / 2 - 1, 22, 0);
-        piezas[1].transform.position = new Vector3(columnas / 2, 22, 0);
-        piezas[2].transform.position = new Vector3(columnas / 2, 21, 0);
-        piezas[3].transform.position = new Vector3(columnas / 2 + 1, 21, 0);
+        piezas[0].transform.position = new Vector3(columnas / 2 - 1, 20, 0);
+        piezas[1].transform.position = new Vector3(columnas / 2, 20, 0);
+        piezas[2].transform.position = new Vector3(columnas / 2, 19, 0);
+        piezas[3].transform.position = new Vector3(columnas / 2 + 1, 19, 0);
     }
 
     void SpawnI()
     {
-        piezas[0].transform.position = new Vector3(columnas / 2, 22, 0);
-        piezas[1].transform.position = new Vector3(columnas / 2, 21, 0);
-        piezas[2].transform.position = new Vector3(columnas / 2, 20, 0);
-        piezas[3].transform.position = new Vector3(columnas / 2, 19, 0);
+        piezas[0].transform.position = new Vector3(columnas / 2, 20, 0);
+        piezas[1].transform.position = new Vector3(columnas / 2, 19, 0);
+        piezas[2].transform.position = new Vector3(columnas / 2, 18, 0);
+        piezas[3].transform.position = new Vector3(columnas / 2, 17, 0);
     }
 
     void SpawnL()
     {
-        piezas[0].transform.position = new Vector3(columnas / 2, 22, 0);
-        piezas[1].transform.position = new Vector3(columnas / 2, 21, 0);
-        piezas[2].transform.position = new Vector3(columnas / 2, 20, 0);
-        piezas[3].transform.position = new Vector3(columnas / 2 + 1, 20, 0);
+        piezas[0].transform.position = new Vector3(columnas / 2, 20, 0);
+        piezas[1].transform.position = new Vector3(columnas / 2, 19, 0);
+        piezas[2].transform.position = new Vector3(columnas / 2, 18, 0);
+        piezas[3].transform.position = new Vector3(columnas / 2 + 1, 18, 0);
     }
 
     void SpawnT()
     {
-        piezas[0].transform.position = new Vector3(columnas / 2 - 1, 22, 0);
-        piezas[1].transform.position = new Vector3(columnas / 2, 22, 0);
-        piezas[2].transform.position = new Vector3(columnas / 2 + 1, 22, 0);
-        piezas[3].transform.position = new Vector3(columnas / 2, 21, 0);
+        piezas[0].transform.position = new Vector3(columnas / 2 - 1, 20, 0);
+        piezas[1].transform.position = new Vector3(columnas / 2, 20, 0);
+        piezas[2].transform.position = new Vector3(columnas / 2 + 1, 20, 0);
+        piezas[3].transform.position = new Vector3(columnas / 2, 19, 0);
     }
 
     void SpawnO()
     {
-        piezas[0].transform.position = new Vector3(columnas / 2 - 1, 22, 0);
-        piezas[1].transform.position = new Vector3(columnas / 2, 22, 0);
-        piezas[2].transform.position = new Vector3(columnas / 2 - 1, 21, 0);
-        piezas[3].transform.position = new Vector3(columnas / 2, 21, 0);
+        piezas[0].transform.position = new Vector3(columnas / 2 - 1, 20, 0);
+        piezas[1].transform.position = new Vector3(columnas / 2, 20, 0);
+        piezas[2].transform.position = new Vector3(columnas / 2 - 1, 19, 0);
+        piezas[3].transform.position = new Vector3(columnas / 2, 19, 0);
     }
 
-    void RotarPieza()
+    Vector3[] RotarPieza()
     {
         // Obtener el centro de rotación (usando el segundo cubo en este caso)
         Vector3 pivot = piezas[1].transform.position;
-
+        Vector3[] posicionesAlRotar = new Vector3[4];
         // Rotar cada cubo alrededor del pivote
         for (int i = 0; i < piezas.Length; i++)
         {
             Vector3 relativePos = piezas[i].transform.position - pivot;
-            piezas[i].transform.position = new Vector3(-relativePos.y + pivot.x, relativePos.x + pivot.y, 0);
+            posicionesAlRotar[i] = new Vector3(-relativePos.y + pivot.x, relativePos.x + pivot.y, 0);
         }
+        return posicionesAlRotar;
+    }
+
+    bool PoderRotarPieza(Vector3[] posicionesAlRotar)
+    {
+        bool resultado = true;
+        for (int i = 0; i < posicionesAlRotar.Length; i++)
+        {
+            if (posicionesAlRotar[i].x <= 0 || posicionesAlRotar[i].y <= 0 || posicionesAlRotar[i].x >= columnas || !posiciones[(int)posicionesAlRotar[i].x, (int)posicionesAlRotar[i].y])
+            {
+                resultado = false;
+                break;
+            }
+        }
+        return resultado;
     }
 }
